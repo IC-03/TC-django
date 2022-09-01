682fe66c9de5d2ceb444b758d655c141550b536b
Isabella <isabella.callejas.m@gmail.com>
2022-09-01 07:01:31 -0500

    Terminando parte 2

--- mysite/libros/views.py
+++ mysite/libros/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render #genera la pag
 
 # Create your views here.
+#imports
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request): #configurado para get
+    #Archivo HTML con template
+    template = loader.get_template('index.html')
+    #logica de la vista
+    context = {} #info de la lógica
+    #respuesta
+    return HttpResponse(template.render(context,request))
